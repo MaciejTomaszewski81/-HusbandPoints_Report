877c2ba40a447625d39cc853070f1eb51fee8e71
soho.tomaszewski@gmail.com <Debiledwa2>
2022-01-19 20:33:37 +0100

    First commit

---
+++ src/main/java/pl/tomaszewski/demo/HusbandPointsReportApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszewski.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HusbandPointsReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HusbandPointsReportApplication.class, args);
+    }
+
+}
